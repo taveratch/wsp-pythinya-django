9be7b5985d3097c44b5950c9ffeffae91b2a51f9
TAWEESOFT <TAWEESOFT@TAWEESOFts-MacBook-Pro.local>
2016-09-09 23:39:04 +0700

    set up react for django

---
+++ hello_world/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from react.render import render_component
+import os
+
+def index(request):
+    rendered = render_component(os.path.join(os.getcwd()+'/hello_world/static/js/hello.jsx'),{},to_static_markup=True)
+    return render(request,'hello_world/index.html', {'component': rendered})
